e33383b65045afc359730581816bf76919909c56
kaito <k.morizumi.p@gmail.com>
2019-05-03 10:19:34 +0000

    Finish layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,12 @@
 module ApplicationHelper
 
   # ページごとにタイトルを返す。
-  def full_title(page_title = "")　#メソッドの設定と定義
-      
-      base_title = "勤怠管理システム"   #定義ベースタイトル
-    if page_title.empty?　#もし空だったら
-      base_title　#勤怠管理システムを表示
-    # 区切る棒が表示されないに対しての対応
+  def full_title(page_title = "") # メソッド名と引数の定義
+    base_title = "勤怠管理システム" # 変数に代入
+    if page_title.empty? # 引数の論理値確認
+      base_title # page_titleが空（""）の時は既に定義してあるbase_titleを返す
     else
-      page_title + " | " + base_title　#それ以外だったら棒を表示
+      page_title + " | " + base_title # 文字列を結合して返す
     end
   end
 end
